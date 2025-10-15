d9d849f779fe3d597439de0fed308802a7953ad6
“liguangran-s” <liguangran-s@inspur.com>
2025-10-15 11:31:27 +0800

    更新publicPath为delta-force-lottery

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/sanjiaozhou/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/delta-force-lottery/' : '/',
   outputDir: 'dist',
   assetsDir: 'static'
 })
